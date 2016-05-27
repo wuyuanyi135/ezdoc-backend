3f552aceb108b953cd6db962ba77b7bc374ec54c
wuyuanyi135 <wuyuanyi135@gmail.com>
2016-05-27 01:37:10 -0400

    suggestion

--- api/services/suggestion.js
+++ api/services/suggestion.js
@@ -15,6 +15,7 @@ const fuzzyOptions = {
     post: '</b>'
 };
 const pmidFuzzyOptions = _.assign({}, fuzzyOptions, {extract: (arg) => arg.pmid});
+const titleOptions = _.assign({}, fuzzyOptions, {extract: (arg) => arg.articleTitle});
 
 var serviceModel = {
     find: function(params) {
@@ -27,8 +28,9 @@ var serviceModel = {
                 return;
             }
             var q = query.q;
-            if (!isNaN(q) && q > 0) {
+            if (!isNaN(q)) {
                 // the query will only return pmid results
+                // Put here rather than above if because other case will query applicant db
                 service.entryService.find({
                     query: {
                         $select: ['_id', 'pmid', 'articleTitle', 'authors']
@@ -37,11 +39,61 @@ var serviceModel = {
                 .then(value => {
                     resolve([{
                         title: "PMID",
-                        matching: fuzzy.filter(q, value, pmidFuzzyOptions)
+                        matching: _(value)
+                                    .map(v => {
+                                        var string
+                                        if ( (new RegExp(`^${q}`)).test(v.pmid)){
+                                            string = v.pmid.replace(`${q}`, `<b>${q}</b>`);
+                                        } else {
+                                            return null;    // so can be filtered by without
+                                        }
+
+                                        return {string, original:v}
+                                    })
+                                    .without(null)
+                                    .take(10)  //change the order to optimize the performance
+
                     }]);
+                }).catch(err => {reject(err)});
+            } else { // if is number
+                // is literatural string
+                var entryPromise = service.entryService.find({
+                    query: {
+                        $select: ['_id', 'pmid', 'articleTitle', 'authors']
+                    }
                 })
-                .catch(err => {reject(err)})
-            }
+                .then(value => {
+                    return [{
+                        title: "Title",
+                        matching: fuzzy.filter(q, value, titleOptions)
+                    }];
+                });
+
+                var applicantPromise = service.applicantService.find({
+                    query: {
+                        $select: ['_id', 'pmid', 'applicant', 'applicantPinyin']
+                    }
+                })
+                .then(value => ({
+                    title: "Applicant",
+                    matching: _(value).map((item) => {
+                                    if (fuzzy.test(q, `${item.applicant} ${item.applicantPinyin}`)) {
+                                        return {string: item.applicant, original:item};
+                                    } else {
+                                        return null;
+                                    }
+                                })
+                                .without(null)
+                                .take(10)
+                    })
+                );
+
+                Promise.all([applicantPromise, entryPromise])
+                    .then((suggestions)=>{
+                        resolve(_.flatten(suggestions));
+                    });
+            } //else
+
         });
     }
 }
