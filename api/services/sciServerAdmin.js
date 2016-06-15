293be9d2542ed95ef2081bf1a3e052d4201905b8
elib <elib>
2016-06-15 09:57:16 +0800

    use tcp port detection to determine whether phantom server is on

--- api/services/sciServerAdmin.js
+++ api/services/sciServerAdmin.js
@@ -1,48 +1,54 @@
 var request = require('request');
 const checkInterval = 5 * 1000 * 60; // 5min
 const url = 'http://localhost:8081/api/sciserver';
+var tcpPortUsed = require('tcp-port-used');
 
 const checkLoginHandler = () => {
-    console.log('checking login status');
-    request(`${url}?action=checklogin`, (err, response, body) => {
-        var _body;
-        try {
-            _body = JSON.parse(body);
-        } catch (e) {
-            console.error('Fail to parse checklogin json', e);
-            console.error('The parsed object is', body);
-            return;
-        }
+    // waiting for port open
+    tcpPortUsed.waitUntilUsed(19190, 1000, 20000)
+    .then(()=>{
+	console.log('port is open');
+        console.log('checking login status');
+        request(`${url}?action=checklogin`, (err, response, body) => {
+            var _body;
+            try {
+                _body = JSON.parse(body);
+            } catch (e) {
+                console.error('Fail to parse checklogin json', e);
+                console.error('The parsed object is', body);
+                return;
+            }
 
-        if (err) {
-            console.log('Fail to communicate with sci server');
-            return;
-        }
+            if (err) {
+                console.log('Fail to communicate with sci server');
+                return;
+            }
 
-        if (_body.isLogin === false) {
-            console.log('not login, start to login');
-            request(`${url}?action=login`, (err, response, body) => {
-                var _body;
-                try {
-                    _body = JSON.parse(body);
-                } catch (e) {
-                    console.log('Fail to parse login json', e);
-                    return;
-                }
+            if (_body.isLogin === false) {
+                console.log('not login, start to login');
+                request(`${url}?action=login`, (err, response, body) => {
+                    var _body;
+                    try {
+                        _body = JSON.parse(body);
+                    } catch (e) {
+                        console.log('Fail to parse login json', e);
+                        return;
+                    }
 
-                if (err) {
-                    console.log('Fail to relogin', _body);
-                    return;
-                }
-                if (_body.status === "success") {
-                    console.log('relogin succeed');
-                } else {
-                    console.log('relogin failed', _body);
-                }
-            });
-        } else {
-            console.log('logged in');
-        }
+                    if (err) {
+                        console.log('Fail to relogin', _body);
+                        return;
+                    }
+                    if (_body.status === "success") {
+                        console.log('relogin succeed');
+                    } else {
+                        console.log('relogin failed', _body);
+                    }
+                });
+            } else {
+                console.log('logged in');
+            }
+        });
     });
 }
 var checkIntervalTimer;
@@ -51,7 +57,7 @@ flushTimer = () => {
     if (! checkIntervalTimer) {
         // first run
         console.log('first run, will login after 1s');
-        setTimeout(checkLoginHandler,8000);
+        setTimeout(checkLoginHandler,1000);
     } else {
         clearInterval(checkIntervalTimer);
     }
